84b58c06b4dd02eab663ecc5b80e71a4e4acda52
heshamahmed <ec22518@qmul.ac.uk>
2025-07-29 15:10:16 +0300

    Initial Spring Boot project

---
+++ src/main/java/com/hertssu/hertssu/HertssuApplication.java
@@ -0,0 +1,13 @@
+package com.hertssu.hertssu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HertssuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HertssuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hertssu/hertssu/HertssuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hertssu.hertssu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HertssuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
